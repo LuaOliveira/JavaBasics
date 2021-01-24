85606af9ce625771b98469997e9171477f1b8283
Luana <luanaos2512@gmail.com>
2021-01-24 12:15:57 -0300

    codigos iniciais em java

---
+++ src/iniciante/Pessoa.java
@@ -0,0 +1,37 @@
+package iniciante;
+
+import com.sun.org.apache.bcel.internal.generic.RETURN;
+
+public class Pessoa {
+
+	// atributos
+	private String nome;
+	private int maxLivros;
+	
+	// construtor
+	public Pessoa() {
+	
+	nome= "Nome desconhecido";
+	maxLivros= 3;
+	
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String qualquerNome) {
+		nome = qualquerNome;
+		
+	}
+
+	public int getMaxLivros() {
+		return maxLivros;
+	}
+
+	public void setMaxLivros(int maxLivros) {
+		//this opcional neste caso
+		this.maxLivros = maxLivros;
+	}
+	
+}
